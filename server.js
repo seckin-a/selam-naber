413916b3c9d02f8a53424866e9c384917725f5dd
Seckin <seckin2x@gmail.com>
2025-06-09 15:24:42 +0300

    v3 commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Selam naber! - HTTP');
+    res.end('Selam naber3! - HTTP');
 });
 
 server.listen(3001, () => {
